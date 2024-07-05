dd2083b0a3e140064284aae7b426cbda2bd96cf4
OddQueen <queensleymei@gmail.com>
2024-07-05 10:51:23 +0200

    observables

--- APIs/src/app/app.module.ts
+++ APIs/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksService } from './shared/books.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksService
   ],
   imports: [
     BrowserModule,
